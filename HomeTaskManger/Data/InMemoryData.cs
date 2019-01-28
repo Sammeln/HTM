563f7bd252abaa900d12c4709122a7119901cd9f
Sammeln <kirilln272@gmail.com>
2019-01-28 17:56:12 +0300

    add model item

---
+++ HomeTaskManger/Data/InMemoryData.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using HomeTaskManger.Model;
+
+namespace HomeTaskManger.Data
+{
+    public class InMemoryData : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private static InMemoryData instance;
+        private static object SyncRoot = new object();
+        private ObservableCollection<ItemModel> items; 
+        #endregion
+
+        #region Props
+        public static InMemoryData Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (SyncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new InMemoryData();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+        public ObservableCollection<ItemModel> Items
+        {
+            get => items;
+            set
+            {
+                items = value;
+                OnPropertyChanged(nameof(Items));
+            }
+        }
+
+        #endregion
+
+        #region Methods
+        public static InMemoryData GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (SyncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new InMemoryData();
+                    }
+                }
+            }
+            return instance;
+        }
+        #endregion
+
+        #region CTOR
+        public InMemoryData()
+        {
+            Items = new ObservableCollection<ItemModel>
+            {
+                new ItemModel
+                {
+                    Id = 1,
+                    Name = "Name"
+                }
+            };
+        }
+        #endregion
+
+        #region NotifyPropChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
+
+    }
+}
---
+++ HomeTaskManger/Model/ItemModel.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace HomeTaskManger.Model
+{
+    public class ItemModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string name;
+        private int id;
+        private ObservableCollection<ItemModel> subItems;
+
+        #endregion
+
+        #region Props
+        
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = "Item #" + Id.ToString();
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+        public ObservableCollection<ItemModel> SubItems
+        {
+            get => subItems;
+            set
+            {
+                subItems = value;
+                OnPropertyChanged(nameof(SubItems));
+            }
+        }
+
+        #endregion
+
+        private void SubCTOR(SubItemLvl itemLvl)
+        {
+
+        }
+        #region NotifyPropChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        } 
+        #endregion
+    }
+    public enum SubItemLvl
+    {
+        None = 0,
+        One,
+        Two,
+        Three
+    }
+}
